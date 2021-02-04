e670c049e3831a878113dc33983f7aed2d3a9a09
Jacob Lee <jacoblee628@gmail.com>
2021-02-03 23:05:39 -0500

    progress

--- train.py
+++ train.py
@@ -5,34 +5,59 @@ from torch.utils.data import DataLoader
 from data import StyleTransferDataset, get_transforms
 import torch
 
+# Determine constants
 cuda = torch.cuda.is_available()
 NUM_WORKERS = os.cpu_count() if cuda else 0
 DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print(f"num_workers: {NUM_WORKERS}, device: {DEVICE}")
 
 def main():
+    # init objects
     encoder = VGG19Encoder()
     encoder.freeze()
     decoder = Decoder()
     print("Created models")
 
     transform = get_transforms()
-    dataset = StyleTransferDataset("/Users/jacoblee628/Documents/Github/AdaIN-reimplementation/coco/train2017",  "/Users/jacoblee628/Documents/Github/AdaIN-reimplementation/coco/annotations/captions_train2017.json", "/Users/jacoblee628/Documents/Github/AdaIN-reimplementation/coco/wikiart", transform)
+    dataset = StyleTransferDataset("coco/train2017",  "/coco/annotations/captions_train2017.json", "/coco/wikiart", transform)
 
     # dataset = StyleTransferDataset("/home/anti/coco/train2017",  "/home/anti/coco/annotations/captions_train2017.json", "/home/anti/coco/wikiart", transform)
     dataloader = DataLoader(dataset, batch_size = 1, num_workers = 0)
     print("Created dataloader")
 
     optimizer = torch.optim.Adam(params = decoder.parameters())
+    
+    
+    # Actual training
+    num_epochs = 1
+    
+    for epoch in range(num_epochs):
+        train_epoch(encoder, decoder, dataloader, optimizer)
+        validate(encoder, decoder, dataloader)
 
+
+def train_epoch(encoder, decoder, dataloader, optimizer):
     for i, (content_image, style_image) in enumerate(dataloader):
         content_image, style_image = content_image.to(DEVICE), style_image.to(DEVICE)
-        #optimizer.zero_grad()
+        optimizer.zero_grad()
 
         loss = get_batch_style_transfer_loss(encoder, decoder, content_image, style_image)
+        
+        print(f"(Debug) Loss:{loss.item()}") # TODO: Remove after dev done
 
         loss.backward()
-        #optimizer.step()
+        optimizer.step()
+
+
+def validate(encoder, decoder, dataloader):
+    encoder.eval(); decoder.eval()
+    
+    with torch.no_grad():
+        for i, (content_image, style_image) in enumerate(dataloader):
+            content_image, style_image = content_image.to(DEVICE), style_image.to(DEVICE)
+    
+    # TODO: Finish
+
 
 
 def get_batch_style_transfer_loss(encoder, decoder, style_image, content_image):

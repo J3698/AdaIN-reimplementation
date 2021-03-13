53e382d4a4f6f767ad3b99bb4813d2981e64e98e
Antioch Sanders <antiochsanders@gmail.com>
2021-03-13 02:05:40 -0500

    Add data download script

---
+++ download_data.sh
@@ -0,0 +1,12 @@
+aria2c --file-allocation=none -c -x 10 -s 10 -d "./" http://web.fsktm.um.edu.my/~cschan/source/ICIP2017/wikiart.zip
+aria2c --file-allocation=none -c -x 10 -s 10 -d "./" http://images.cocodataset.org/zips/train2017.zip
+aria2c --file-allocation=none -c -x 10 -s 10 -d "./" http://images.cocodataset.org/zips/val2017.zip
+aria2c --file-allocation=none -c -x 10 -s 10 -d "./" http://images.cocodataset.org/annotations/annotations_trainval2017.zip
+
+mkdir datasets
+mv train2017.zip datasets
+mv val2017.zip datasets
+mv annotations_trainval2017.zip datasets
+mv wikiart.zip datasets
+cd datasets
+unzip *.zip

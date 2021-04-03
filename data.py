f2dd7321c3a74ef43a3abed6d95f92fb372312f1
Antioch Sanders <antiochsanders@gmail.com>
2021-04-03 00:24:34 +0000

    Add iterable style dataset

--- data.py
+++ data.py
@@ -1,28 +1,119 @@
 import torch
 import warnings
 import torchvision
-from torch.utils.data import Dataset
+from torch.utils.data import Dataset, IterableDataset, DataLoader
 from torchvision.datasets.folder import default_loader
 import os
 from torchvision.transforms import ToTensor, RandomCrop, Resize, Compose
 from torchvision.datasets import ImageFolder, CocoCaptions
 from random import randrange, seed
+import random
+from math import ceil
 
 
 def main():
     transforms = get_transforms()
-    dataset = StyleTransferDataset("datasets/coco/train2017",  "datasets/coco/annotations/captions_train2017.json", "datasets/wikiart", transform = transforms)
+
+    coco_path = "datasets/train2017"
+    coco_annot = "datasets/annotations/captions_train2017.json"
+    wiki_path = "datasets/wikiart"
+    dataset = StyleTransferDataset(coco_path, coco_annot, wiki_path, transform = transforms)
     print(f"Dataset length: {len(dataset)}, Wiki length: {len(dataset.wiki)}, COCO length: {len(dataset.coco)}")
 
     content, style = dataset[0]
     print(f"1st content img: {type(content)}, {content.shape}")
     print(f"1st style img: {type(style)}, {style.shape}")
 
+    dataset = IterableStyleTransferDataset(coco_path, coco_annot, wiki_path, transform = transforms)
+    print(f"Dataset length: {len(dataset)}, Wiki length: {len(dataset.wiki)}, COCO length: {len(dataset.coco)}")
+
+    a = DataLoader(dataset, num_workers = 2)
+    it = iter(a)
+    content1, style1 = next(it)
+    content2, style2 = next(it)
+    assert not torch.all(content1 == content2)
+    assert not torch.all(style1 == style2)
+    print(f"1st content img: {type(content1)}, {content1.shape}")
+    print(f"1st style img: {type(style1)}, {style1.shape}")
+
+
+    dataset = IterableStyleTransferDataset(coco_path, coco_annot, wiki_path, transform = transforms, length = 323)
+    j = 0
+    for x, y in DataLoader(dataset, num_workers = 4): 
+        j += len(x)
+    assert j == 323, j
+
+
 
 def get_transforms():
     return Compose([Resize((256, 256)), ToTensor()])
     # return Compose([Resize((256, 256)), ToTensor()])
 
+
+class IterableStyleTransferDataset(IterableDataset):
+    def __init__(self, coco_path, coco_annotations, \
+                 wiki_path, length = 100000, transform = None, rng_seed = 1, exclude_style = False):
+
+        self.wiki = ImageFolder(wiki_path, transform = transform)
+        self.coco = CocoCaptions(coco_path, coco_annotations, transform = transform)
+        self.length = length
+        self.exclude_style = exclude_style
+
+        self.seed = rng_seed
+        self.random = random.Random(rng_seed)
+        print(length)
+        self.indices = [self.random_pair_of_indices() for i in range(length)]
+
+
+    def random_pair_of_indices(self):
+        return self.random.randrange(len(self.coco)),\
+               self.random.randrange(len(self.wiki))
+
+    def __iter__(self):
+        worker_info = torch.utils.data.get_worker_info()
+        if worker_info is not None:
+            self.random.seed(self.seed + worker_info.id)
+
+            length = ceil(self.length // worker_info.num_workers)
+            if worker_info.id == worker_info.num_workers - 1:
+                length = self.length - length * (worker_info.num_workers - 1)
+                assert length > 0
+            self.ilength = length
+        else:
+            self.ilength = length
+
+        return self
+
+    def __len__(self):
+        return self.length
+
+    def __next__(self):
+        if self.ilength <= 0:
+            raise StopIteration
+
+        self.ilength -= 1
+
+        coco_idx, wiki_idx = self.random_pair_of_indices()
+
+        content_image = self.coco[coco_idx][0]
+        if not self.exclude_style:
+            style_image = self.wiki[wiki_idx][0]
+        else:
+            style_image = None
+
+        self._check_data_to_return(style_image, content_image)
+
+        if self.exclude_style:
+            return content_image
+        else:
+            return content_image, style_image
+
+
+    def _check_data_to_return(self, style_image, content_image):
+        if not (style_image is None or isinstance(style_image, torch.Tensor)) or \
+           not (content_image is None or isinstance(content_image, torch.Tensor)):
+            warnings.warn("Given transform does not convert images to tensors;"
+                          "default collate may fail.", UserWarning)
 
 class StyleTransferDataset(Dataset):
     def __init__(self, coco_path, coco_annotations, \
--- train.py
+++ train.py
@@ -2,9 +2,10 @@ from encoder import VGG19Encoder
 from decoder import Decoder
 import os
 from adain import adain
+from math import ceil
 import tqdm
 from torch.utils.data import DataLoader
-from data import StyleTransferDataset, get_transforms
+from data import IterableStyleTransferDataset, get_transforms
 import torch
 import torch.optim as optim
 import matplotlib.pyplot as plt
@@ -18,7 +19,7 @@ cuda = torch.cuda.is_available()
 NUM_WORKERS = os.cpu_count() if cuda else 0
 DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 BATCH_SIZE = 16 if cuda else 3
-DATASET_LENGTH = 2 if cuda else 1
+DATASET_LENGTH = 1000 if cuda else 1
 NUM_EPOCHS = 10000
 LR = 1e-3
 LAMBD_STYLE = 0.1
@@ -52,13 +53,13 @@ def main():
     print("Created models")
 
     transform = get_transforms()
-    dataset = StyleTransferDataset(COCO_PATH, COCO_LABELS_PATH,\
+    dataset = IterableStyleTransferDataset(COCO_PATH, COCO_LABELS_PATH,\
                                    WIKIART_PATH, length = DATASET_LENGTH,
                                    transform = transform, exclude_style = False,
                                    rng_seed = SEED)
 
     dataloader = DataLoader(dataset, batch_size = BATCH_SIZE,
-                            num_workers = NUM_WORKERS, shuffle = True)
+                            num_workers = NUM_WORKERS)
     print("Created dataloader")
 
     optimizer = torch.optim.Adam(params = decoder.parameters(), lr = LR)
@@ -67,8 +68,22 @@ def main():
 
     run = time.time()
     os.makedirs(f"demo/{run}")
+    loss = None
     for epoch in range(NUM_EPOCHS):
-        # torch.save(decoder, f"demo/{run}/enc-{epoch}.pt")
+        torch.save({
+            'epoch': epoch,
+            'decoder_state_dict': decoder.state_dict(),
+            'optimizer_state_dict': optimizer.state_dict(),
+            'scheduler_state_dict': scheduler.state_dict(),
+            'loss': loss,
+            'num_workers': NUM_WORKERS, 'device': DEVICE,
+            'batch_size': BATCH_SIZE, 'dataset_length': DATASET_LENGTH,
+            'num_epochs': NUM_EPOCHS, 'lr': LR,
+            'lambda_style': LAMBD_STYLE, 'lambda_content': LAMBD_CONTENT,
+            'seed': SEED, 'coco_path': COCO_PATH,
+            'coco_labels_path': COCO_LABELS_PATH, 'wiki_path': WIKIART_PATH,
+        }, f"demo/{run}/{epoch}.pt")
+
         loss = train_epoch_style_loss(encoder, decoder, dataloader, optimizer, epoch, writer, run)
         scheduler.step(loss)
 
@@ -80,66 +95,37 @@ def train_epoch_style_loss(encoder, decoder, dataloader, optimizer, epoch_num, w
     decoder.train()
 
     total_loss = 0
-    progress_bar = tqdm.tqdm(enumerate(dataloader), total = len(dataloader), dynamic_ncols = True)
+    num_iters = ceil(len(dataloader) / BATCH_SIZE)
+    progress_bar = tqdm.tqdm(enumerate(dataloader), total = num_iters, dynamic_ncols = True)
     for i, (content_image, style_image) in progress_bar:
 
         content_image, style_image = content_image.to(DEVICE), style_image.to(DEVICE)
         g_batch_size, _, w, h = content_image.shape
-        assert content_image.shape == style_image.shape
+        # assert content_image.shape == style_image.shape
 
         optimizer.zero_grad()
         style_loss, content_loss, stylized = get_batch_style_transfer_loss(encoder, decoder, \
                                                                            content_image, style_image)
-        (style_loss + content_loss).backward()
+        full_loss = style_loss + content_loss
+        full_loss.backward()
 
-        total_loss += (style_loss + content_loss).item()
+        total_loss += full_loss.item()
 
         progress_bar.set_postfix({'epoch': f"{epoch_num}", 'loss': f"{total_loss / (i + 1):.2f}"})
 
         if i % 300 == 0:
-            step = (len(dataloader) // 300 + 1) * epoch_num + i // 300
-            if epoch_num % 10 == 0:
+            step = (num_iters // 300 + 1) * epoch_num + i // 300
+            if epoch_num % 5 == 0:
                 writer.add_image('style', prep_img_for_tb(style_image[0]), step)
                 writer.add_image('content', prep_img_for_tb(content_image[0]), step)
                 writer.add_image('stylized', prep_img_for_tb(stylized[0]), step)
 
-                # do it separately
-                g_batch_size = 1
-
-                content1 = dataloader.dataset[0][0][None].cuda()
-                style1 = dataloader.dataset[0][1][None].cuda()
-                assert content1.shape == (1, 3, 256, 256)
-                style_loss1, content_loss1, stylized1 = \
-                    get_batch_style_transfer_loss(encoder, decoder, content1, style1)
-
-                content2 = dataloader.dataset[1][0][None].cuda()
-                style2 = dataloader.dataset[1][1][None].cuda()
-                style_loss2, content_loss2, stylized2 = \
-                    get_batch_style_transfer_loss(encoder, decoder, content2, style2)
-
-                it = epoch_num * len(dataloader) + i
-                writer.add_scalar('SLoss/train_it1', style_loss1.item(), it)
-                writer.add_scalar('CLoss/train_it1', content_loss1.item(), it)
-                writer.add_scalar('SLoss/train_it2', style_loss2.item(), it)
-                writer.add_scalar('CLoss/train_it2', content_loss2.item(), it)
-                writer.add_image('stylized1', prep_img_for_tb(stylized1[0]), step)
-                writer.add_image('stylized2', prep_img_for_tb(stylized2[0]), step)
-                writer.add_scalar('Loss/diff',
-                    (content_loss - (content_loss1 + content_loss2) / 2 + \
-                     style_loss - (style_loss1 + style_loss2) / 2).item() , it)
-                diff1 = (stylized - torch.cat((stylized1, stylized2))).mean()
-                diff2 = (stylized - torch.cat((stylized2, stylized1))).mean()
-                out_diff = torch.min(diff1, diff2)
-                print(out_diff)
-
-                  
-
         optimizer.step()
 
-        it = epoch_num * len(dataloader) + i
+        it = epoch_num * num_iters + i
         writer.add_scalar('SLoss/train_it', style_loss.item(), it)
         writer.add_scalar('CLoss/train_it', content_loss.item(), it)
-        writer.add_scalar('Loss/train_it', (content_loss + style_loss).item(), it)
+        writer.add_scalar('Loss/train_it', full_loss.item(), it)
 
 
 
@@ -149,14 +135,14 @@ def train_epoch_style_loss(encoder, decoder, dataloader, optimizer, epoch_num, w
 def prep_img_for_tb(image):
     copy = image.cpu().detach().clone()
     clamp = torch.clamp(copy, 0, 1)
-    assert clamp.shape == (3, 256, 256)
+    # assert clamp.shape == (3, 256, 256)
 
     return clamp
 
 
 def get_batch_style_transfer_loss(encoder, decoder, content_image, style_image,
                                   lambds = LAMBD_STYLE, lambdc = LAMBD_CONTENT):
-    assert content_image.shape == (g_batch_size, 3, 256, 256)
+    # assert content_image.shape == (g_batch_size, 3, 256, 256)
 
     style_features = encoder(style_image)
     content_features = encoder(content_image)
@@ -172,42 +158,44 @@ def get_batch_style_transfer_loss(encoder, decoder, content_image, style_image,
 
 
 def create_stylized_images(decoder, content_features, style_features):
-    assert len(content_features) == 4
-    assert len(style_features) == 4
+    # assert len(content_features) == 4
+    # assert len(style_features) == 4
 
+    """
     shapes = [i.shape for i in content_features]
-    assert shapes == [(g_batch_size, 64, 256, 256),
+     assert shapes == [(g_batch_size, 64, 256, 256),
                       (g_batch_size, 128, 128, 128),
                       (g_batch_size, 256, 64, 64),
                       (g_batch_size, 512, 32, 32) ], shapes
     shapes = [i.shape for i in style_features]
-    assert shapes == [(g_batch_size, 64, 256, 256),
+    # assert shapes == [(g_batch_size, 64, 256, 256),
                       (g_batch_size, 128, 128, 128),
                       (g_batch_size, 256, 64, 64),
                       (g_batch_size, 512, 32, 32) ], shapes
+    """
 
     stylized_features = adain(content_features[-1], style_features[-1])
-    assert stylized_features.shape == (g_batch_size, 512, 32, 32)
+    # assert stylized_features.shape == (g_batch_size, 512, 32, 32)
 
     stylized_images = decoder(stylized_features)
-    assert stylized_images.shape == (g_batch_size, 3, 256, 256)
+    # assert stylized_images.shape == (g_batch_size, 3, 256, 256)
 
     return stylized_images, stylized_features
 
 
 def compute_content_loss(features_of_stylized, stylized_features):
-    assert features_of_stylized.shape == (g_batch_size, 512, 32, 32)
-    assert stylized_features.shape == (g_batch_size, 512, 32, 32)
+    # assert features_of_stylized.shape == (g_batch_size, 512, 32, 32)
+    # assert stylized_features.shape == (g_batch_size, 512, 32, 32)
 
     batch_size = features_of_stylized.shape[0]
 
     content_loss = F.mse_loss(features_of_stylized, stylized_features, reduction = "none")
-    assert content_loss.shape == (g_batch_size, 512, 32, 32), content_loss.shape
+    # assert content_loss.shape == (g_batch_size, 512, 32, 32), content_loss.shape
 
     content_loss = content_loss.view(batch_size, -1)
-    assert content_loss.shape == (batch_size, 512 * 32 * 32)
+    # assert content_loss.shape == (batch_size, 512 * 32 * 32)
     content_loss = content_loss.sum(-1) ** 0.5
-    assert content_loss.shape == (batch_size,)
+    # assert content_loss.shape == (batch_size,)
 
     return content_loss.mean()
 
@@ -215,6 +203,7 @@ def compute_content_loss(features_of_stylized, stylized_features):
 def compute_style_loss(features_of_stylized, style_features):
     style_loss = 0
 
+    """
     shapes = [i.shape for i in features_of_stylized]
     assert shapes == [(g_batch_size, 64, 256, 256),
                       (g_batch_size, 128, 128, 128),
@@ -226,6 +215,7 @@ def compute_style_loss(features_of_stylized, style_features):
                       (g_batch_size, 128, 128, 128),
                       (g_batch_size, 256, 64, 64),
                       (g_batch_size, 512, 32, 32) ], shapes
+    """
 
     zipped_features = zip(features_of_stylized, style_features)
     for feat_of_stylized, style_feat in zipped_features:
@@ -237,29 +227,29 @@ def compute_style_loss(features_of_stylized, style_features):
 
         var_loss_vector = var_loss_vector.view(g_batch_size, -1)
         var_losses = var_loss_vector.sum(-1)
-        assert var_losses.shape == (g_batch_size,)
+        # assert var_losses.shape == (g_batch_size,)
         style_loss += var_losses.mean()
 
         mean_loss_vector = mean_loss_vector.view(g_batch_size, -1)
         mean_losses = mean_loss_vector.sum(-1)
-        assert mean_losses.shape == (g_batch_size,)
+        # assert mean_losses.shape == (g_batch_size,)
         style_loss += mean_losses.mean()
 
     return style_loss
 
 
 def calc_feature_stats_vectors(features):
-    assert len(features.shape) == 4
+    # assert len(features.shape) == 4
     batch_size, feature_maps, w, h = features.shape
-    assert batch_size == g_batch_size
+    # assert batch_size == g_batch_size
 
     features = features.view(batch_size * feature_maps, -1)
-    assert features.shape == (g_batch_size * feature_maps, w * h)
+    # assert features.shape == (g_batch_size * feature_maps, w * h)
 
     feature_vars = features.var(-1, unbiased = False)
     feature_means = features.mean(-1)
-    assert feature_vars.shape == (batch_size * feature_maps,), feature_vars.shape
-    assert feature_means.shape == (batch_size * feature_maps,), feature_means.shape
+    # assert feature_vars.shape == (batch_size * feature_maps,), feature_vars.shape
+    # assert feature_means.shape == (batch_size * feature_maps,), feature_means.shape
 
     feature_vars = feature_vars.reshape(batch_size, feature_maps)
     feature_means = feature_means.reshape(batch_size, feature_maps)

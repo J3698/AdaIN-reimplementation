67c2c0c03fca55acd6b8ac46a6c1dee6ba94152b
Antioch Sanders <antiochsanders@gmail.com>
2021-05-18 15:30:52 +0000

    Refactor reconstruction out

--- train.py
+++ train.py
@@ -39,7 +39,7 @@ def main():
     parser.add_argument('--num-epochs', type = int, default = 10000)
     parser.add_argument('--save-freq', type = int, default = 10)
     parser.add_argument('--lr', type = float, default = 1e-3)
-    parser.add_argument('--lambda-style', type = float, default = 3.5e3)
+    parser.add_argument('--lambda-style', type = float, default = 30)
     parser.add_argument('--lambda-content', type = float, default = 1)
     parser.add_argument('--seed', type = int, default = 3033157)
     parser.add_argument('--coco-path', type = str,
@@ -48,6 +48,8 @@ def main():
                         default = "datasets/annotations/captions_train2017.json")
     parser.add_argument('--wikiart-path', type = str,
                         default = "datasets/wikiart")
+    parser.add_argument('--scheduler-step', type = int,
+                        default = 200)
     args = parser.parse_args()
 
     assert args.cpu != args.cuda
@@ -55,6 +57,7 @@ def main():
 
     if args.checkpoint is None:
         NUM_WORKERS = args.num_workers
+        SCHEDULER_STEP = args.scheduler_step
         DEVICE = torch.device(args.device)
         SAVE_FREQ = args.save_freq
         BATCH_SIZE = args.batch_size
@@ -163,17 +166,16 @@ def main():
                                 num_workers = 1, shuffle = True)
     print("Created dataloader")
 
+    if args.lr:
+        LR = args.lr
     optimizer = torch.optim.Adam(params = decoder.parameters(), lr = LR)
     if optimizer_state_dict is not None:
         optimizer.load_state_dict(optimizer_state_dict)
-    if args.lr:
-        for i in optimizer.param_groups:
-            i['lr'] = args.lr
-        print(args.lr)
 
     #scheduler = ReduceLROnPlateau(optimizer, patience = 30,
     #                              verbose = True, threshold = 0)
-    scheduler = StepLR(optimizer, step_size = 100, gamma = 0.1)
+    scheduler = StepLR(optimizer, step_size = args.scheduler_step,\
+                       gamma = 0.1)
     if scheduler_state_dict is not None:
         scheduler.load_state_dict(scheduler_state_dict)
 
@@ -196,7 +198,7 @@ def main():
     os.makedirs(f"demo/{run}")
     loss = None
     for epoch in range(saved_epoch, NUM_EPOCHS):
-        if SAVE_FREQ != 0 and epoch % SAVE_FREQ == 0:
+        if SAVE_FREQ != 0 and epoch % SAVE_FREQ == 0 and False:
             torch.save({
                 'decoder_state_dict': decoder.state_dict(),
                 'optimizer_state_dict': optimizer.state_dict(),
@@ -255,12 +257,12 @@ def train_epoch_style_loss(encoder, decoder, dataloader,\
         optimizer.step()
 
         it = epoch_num * num_iters + i
-        if i % 500 == 0:
-        #if epoch_num % 20 == 0:
+        #if i % 500 == 0:
+        if epoch_num % 20 == 0:
             imgs_per_epoch = num_iters // 500
             img_num = imgs_per_epoch * epoch_num + i // 500
-            validate_style_loss(encoder, decoder, val_dataloader, img_num, writer)
-            #validate_style_loss(encoder, decoder, val_dataloader, epoch_num, writer)
+            #validate_style_loss(encoder, decoder, val_dataloader, img_num, writer)
+            validate_style_loss(encoder, decoder, val_dataloader, epoch_num, writer)
             encoder.train()
             decoder.train()
 
@@ -282,7 +284,7 @@ def prep_img_for_tb(image):
 
 
 def get_style_transfer_loss(encoder, decoder, content_image, style_image):
-    # assert content_image.shape == (g_batch_size, 3, 256, 256)
+    assert content_image.shape == (g_batch_size, 3, 256, 256)
 
     style_features = encoder(style_image)
     content_features = encoder(content_image)
@@ -300,97 +302,109 @@ def get_style_transfer_loss(encoder, decoder, content_image, style_image):
                                      stylized_images
 
 
-def create_stylized_images(decoder, content_features, style_features):
-    # assert len(content_features) == 4
-    # assert len(style_features) == 4
+def style_transfer(encoder, decoder, content_image, style_image):
+    style_features = encoder(style_image)
+    content_features = encoder(content_image)
+    stylized_features = adain(content_features[-1], style_features[-1])
+    stylized_images = decoder(stylized_features)
+    return stylized_images
 
-    """
+
+def create_stylized_images(decoder, content_features, style_features):
+    assert len(content_features) == 4
+    assert len(style_features) == 4
+
     shapes = [i.shape for i in content_features]
-     assert shapes == [(g_batch_size, 64, 256, 256),
+    assert shapes == [(g_batch_size, 64, 256, 256),
                       (g_batch_size, 128, 128, 128),
                       (g_batch_size, 256, 64, 64),
                       (g_batch_size, 512, 32, 32) ], shapes
     shapes = [i.shape for i in style_features]
-    # assert shapes == [(g_batch_size, 64, 256, 256),
+    assert shapes == [(g_batch_size, 64, 256, 256),
                       (g_batch_size, 128, 128, 128),
                       (g_batch_size, 256, 64, 64),
                       (g_batch_size, 512, 32, 32) ], shapes
-    """
 
     stylized_features = adain(content_features[-1], style_features[-1])
-    # assert stylized_features.shape == (g_batch_size, 512, 32, 32)
+    assert stylized_features.shape == (g_batch_size, 512, 32, 32), stylized_features.shape
 
     stylized_images = decoder(stylized_features)
-    # assert stylized_images.shape == (g_batch_size, 3, 256, 256)
+    assert stylized_images.shape == (g_batch_size, 3, 256, 256)
 
     return stylized_images, stylized_features
 
 
 def compute_content_loss(features_of_stylized, stylized_features):
-    # assert features_of_stylized.shape == (g_batch_size, 512, 32, 32)
-    # assert stylized_features.shape == (g_batch_size, 512, 32, 32)
+    assert features_of_stylized.shape == (g_batch_size, 512, 32, 32)
+    assert stylized_features.shape == (g_batch_size, 512, 32, 32)
 
     batch_size = features_of_stylized.shape[0]
 
     content_loss = \
         F.mse_loss(features_of_stylized, stylized_features, reduction = "none")
-    # assert content_loss.shape == (g_batch_size, 512, 32, 32), content_loss.shape
+    assert content_loss.shape == (g_batch_size, 512, 32, 32), content_loss.shape
 
     content_loss = content_loss.view(batch_size, -1)
-    # assert content_loss.shape == (batch_size, 512 * 32 * 32)
-    content_loss = content_loss.sum(-1) ** 0.5
-    # assert content_loss.shape == (batch_size,)
+    assert content_loss.shape == (batch_size, 512 * 32 * 32)
 
+    content_loss = content_loss.sum(-1) ** 0.5
+    assert content_loss.shape == (batch_size,)
     return content_loss.mean()
 
 
 def compute_style_loss(features_of_stylized, style_features):
     style_loss = 0
 
-    """
     shapes = [i.shape for i in features_of_stylized]
     assert shapes == [(g_batch_size, 64, 256, 256),
                       (g_batch_size, 128, 128, 128),
                       (g_batch_size, 256, 64, 64),
                       (g_batch_size, 512, 32, 32) ], shapes
+
+    shapes = [i.shape for i in style_features]
     assert shapes == [(g_batch_size, 64, 256, 256),
                       (g_batch_size, 128, 128, 128),
                       (g_batch_size, 256, 64, 64),
                       (g_batch_size, 512, 32, 32) ], shapes
-    """
 
     zipped_features = zip(features_of_stylized, style_features)
     for feat_of_stylized, style_feat in zipped_features:
+        feature_maps = feat_of_stylized.shape[1]
+        assert feat_of_stylized.shape == (g_batch_size, feature_maps,\
+                                          2 ** 14 / feature_maps, 2 ** 14 / feature_maps)
+        assert style_feat.shape == (g_batch_size, feature_maps,\
+                                    2 ** 14 / feature_maps, 2 ** 14 / feature_maps)
+
         stdevs1, means1 = calc_feature_stats_vectors(feat_of_stylized)
         stdevs2, means2 = calc_feature_stats_vectors(style_feat)
+        assert stdevs1.shape == (g_batch_size, feature_maps)
+        assert stdevs2.shape == (g_batch_size, feature_maps)
+        assert means1.shape == (g_batch_size, feature_maps)
+        assert means2.shape == (g_batch_size, feature_maps)
 
-        stdev_loss_vector = F.mse_loss(stdevs1, stdevs2, reduction = "none")
-        mean_loss_vector = F.mse_loss(means1, means2, reduction = "none")
+        stdev_loss_vector = F.mse_loss(stdevs1, stdevs2, reduction = "none") ** 0.5
+        mean_loss_vector = F.mse_loss(means1, means2, reduction = "none") ** 0.5
+        assert stdev_loss_vector.shape == (g_batch_size, feature_maps)
+        assert mean_loss_vector.shape == (g_batch_size, feature_maps)
 
-        stdev_loss_vector = stdev_loss_vector.view(g_batch_size, -1)
-        stdev_losses = stdev_loss_vector.mean(-1)
-
-        mean_loss_vector = mean_loss_vector.view(g_batch_size, -1)
-        mean_losses = mean_loss_vector.mean(-1)
-
-        style_loss += (mean_losses + stdev_losses).mean(-1)
-        # assert mean_losses.shape == (g_batch_size,)
+        style_loss += mean_loss_vector.sum(-1).mean()
+        style_loss += stdev_loss_vector.sum(-1).mean()
 
     return style_loss
 
 
 def calc_feature_stats_vectors(features):
-    # assert len(features.shape) == 4
+    assert len(features.shape) == 4
     batch_size, feature_maps, w, h = features.shape
-    # assert batch_size == g_batch_size
+    assert batch_size == g_batch_size
 
     features = features.view(batch_size * feature_maps, -1)
-    # assert features.shape == (g_batch_size * feature_maps, w * h)
+    assert features.shape == (g_batch_size * feature_maps, w * h)
 
     feature_stdevs = features.var(-1, unbiased = False) ** 0.5
     feature_means = features.mean(-1)
-    # assert feature_vars.shape == (batch_size * feature_maps,), feature_vars.shape
-    # assert feature_means.shape == (batch_size * feature_maps,), feature_means.shape
+    assert feature_stdevs.shape == (batch_size * feature_maps,), feature_vars.shape
+    assert feature_means.shape == (batch_size * feature_maps,), feature_means.shape
 
     feature_stdevs = feature_stdevs.reshape(batch_size, feature_maps)
     feature_means = feature_means.reshape(batch_size, feature_maps)
@@ -419,8 +433,7 @@ def validate_style_loss(encoder, decoder, dataloader, epoch_num, writer):
     style_image = style_image.to(DEVICE)
     g_batch_size, _, w, h = content_image.shape
 
-    _, _, stylized = get_style_transfer_loss(\
-                            encoder, decoder, content_image, style_image)
+    stylized = style_transfer(encoder, decoder, content_image, style_image)
 
     s_image = prep_img_for_tb(style_image[0])
     writer.add_image('style', s_image, epoch_num)
@@ -430,39 +443,6 @@ def validate_style_loss(encoder, decoder, dataloader, epoch_num, writer):
     writer.add_image('stylized', f_image, epoch_num)
 
 
-def train_epoch_reconstruct(encoder, decoder, dataloader, optimizer, epoch_num, writer, run):
-    encoder.train()
-    decoder.train()
-
-    total_loss = 0
-    for i, content_image in tqdm.tqdm(enumerate(dataloader),\
-                                      total = len(dataloader),\
-                                      dynamic_ncols = True):
-        content_image = content_image.to(DEVICE)
-
-        optimizer.zero_grad()
-        reconstruction = decoder(encoder(content_image)[-1])
-
-        if i % 300 == 0:
-            show_tensor(reconstruction[0].detach().clone(),\
-                        epoch_num, run, info = "recon1")
-            show_tensor(content_image[0].detach().clone(),\
-                        epoch_num, run, info = "orgnl1")
-            show_tensor(reconstruction[1].detach().clone(),\
-                        epoch_num, run, info = "recon2")
-            show_tensor(content_image[1].detach().clone(),\
-                        epoch_num, run, info = "orgnl2")
-
-        loss = F.mse_loss(content_image, reconstruction)
-        loss.backward()
-        optimizer.step()
-
-        total_loss += loss.item()
-        writer.add_scalar('Loss/train_it', loss.item(), epoch_num)
-
-    writer.add_scalar('Loss/train', total_loss, epoch_num)
-    print(f"Epoch {epoch_num}, Loss {total_loss}")
-    return total_loss
 
 
 if __name__ == "__main__":

9704f40ec8815e8fcdc3a3455ac25150f82c7d98
Antioch Sanders <antiochsanders@gmail.com>
2021-05-18 18:37:22 -0400

    Refactor train into main and train, undo crop change

--- train.py
+++ train.py
@@ -9,7 +9,6 @@ from data import StyleTransferDataset, IterableStyleTransferDataset, get_transfo
 import torch
 import torch.optim as optim
 from torch.optim.lr_scheduler import ReduceLROnPlateau, StepLR
-import matplotlib.pyplot as plt
 import torch.nn.functional as F
 import time
 import random
@@ -17,210 +16,32 @@ from torch.utils.tensorboard import SummaryWriter
 import argparse
 
 
-def main():
-    global COCO_LABELS_PATH, SEED, BATCH_SIZE, COCO_LABELS_PATH, WIKIART_PATH,\
-            NUM_WORKERS, DEVICE, DATASET_LENGTH, LR, LAMBD_STYLE, COCO_PATH, NUM_EPOCHS,\
-            LAMBD_CONTENT, LAMBD_STYLE, CROP
-    has_cuda = torch.cuda.is_available()
-    action='store_true'
-    parser = argparse.ArgumentParser(description='Train the model :)')
-    parser.add_argument('comment', type = str, help = 'run comment')
-    parser.add_argument('--checkpoint', type = str, default=None,
-                        help='file to load training from')
-    parser.add_argument('--num-workers', type = int,
-                        default = os.cpu_count() if has_cuda else 0)
-    parser.add_argument('--cpu', default = not has_cuda, action='store_true')
-    parser.add_argument('--cuda', default = has_cuda, action='store_true')
-    parser.add_argument('--crop', default = False, action='store_true')
-    parser.add_argument('--batch-size', type = int,
-                        default = 16 if has_cuda else 2)
-    parser.add_argument('--dataset-length', type = int,
-                        default = 1000 if has_cuda else 1)
-    parser.add_argument('--num-epochs', type = int, default = 10000)
-    parser.add_argument('--save-freq', type = int, default = 10)
-    parser.add_argument('--lr', type = float, default = 1e-3)
-    parser.add_argument('--lambda-style', type = float, default = 30)
-    parser.add_argument('--lambda-content', type = float, default = 1)
-    parser.add_argument('--seed', type = int, default = 3033157)
-    parser.add_argument('--coco-path', type = str,
-                        default = "datasets/train2017")
-    parser.add_argument('--coco-labels-path', type = str,
-                        default = "datasets/annotations/captions_train2017.json")
-    parser.add_argument('--wikiart-path', type = str,
-                        default = "datasets/wikiart")
-    parser.add_argument('--scheduler-step', type = int,
-                        default = 200)
-    args = parser.parse_args()
-
-    assert args.cpu != args.cuda
-    args.device = "cpu" if args.cpu else "cuda"
-
-    if args.checkpoint is None:
-        NUM_WORKERS = args.num_workers
-        SCHEDULER_STEP = args.scheduler_step
-        DEVICE = torch.device(args.device)
-        SAVE_FREQ = args.save_freq
-        BATCH_SIZE = args.batch_size
-        DATASET_LENGTH = args.dataset_length
-        NUM_EPOCHS = args.num_epochs
-        LR = args.lr
-        LAMBD_STYLE = args.lambda_style
-        LAMBD_CONTENT = args.lambda_content
-        SEED = args.seed
-        CROP = args.crop
-        torch.random.manual_seed(SEED)
-
-        print(f"num_workers: {NUM_WORKERS}, device: {DEVICE}")
-
-        COCO_PATH = args.coco_path
-        COCO_LABELS_PATH = args.coco_labels_path
-        WIKIART_PATH = args.wikiart_path
-        optimizer_state_dict = None
-        scheduler_state_dict = None
-        decoder_state_dict = None
-        saved_epoch = 0
-    else:
-        checkpoint = torch.load(args.checkpoint)
-
-        optimizer_state_dict = checkpoint['optimizer_state_dict']
-        del checkpoint['optimizer_state_dict']
-        scheduler_state_dict = checkpoint['scheduler_state_dict']
-        del checkpoint['scheduler_state_dict']
-        decoder_state_dict = checkpoint['decoder_state_dict']
-        del checkpoint['decoder_state_dict']
-        saved_epoch = checkpoint['epoch']
-        del checkpoint['epoch']
-        print(f"Starting loss: {checkpoint['loss']}")
-        del checkpoint['loss']
-        NUM_WORKERS = checkpoint['num_workers']
-        del checkpoint['num_workers']
-        BATCH_SIZE = checkpoint['batch_size']
-        del checkpoint['batch_size']
-        NUM_EPOCHS = checkpoint['num_epochs']
-        del checkpoint['num_epochs']
-        LAMBD_STYLE = checkpoint['lambda_style']
-        del checkpoint['lambda_style']
-        SEED = checkpoint['seed'] + 1
-        del checkpoint['seed']
-        COCO_LABELS_PATH = checkpoint['coco_labels_path']
-        del checkpoint['coco_labels_path']
-        DATASET_LENGTH = checkpoint['dataset_length']
-        del checkpoint['dataset_length']
-        LR = checkpoint['lr']
-        del checkpoint['lr']
-        COCO_PATH = checkpoint['coco_path']
-        del checkpoint['coco_path']
-        WIKIART_PATH = checkpoint['wiki_path']
-        del checkpoint['wiki_path']
-        LAMBD_CONTENT = checkpoint['lambda_content']
-        del checkpoint['lambda_content']
-        DEVICE = torch.device(checkpoint['device'])
-        if not torch.cuda.is_available():
-            DEVICE = torch.device('cpu')
-        del checkpoint['device']
-        if 'crop' in checkpoint:
-            CROP = checkpoint['crop']
-            del checkpoint['crop']
-        else:
-            CROP = True
-        if 'save_freq' in checkpoint:
-            SAVE_FREQ = checkpoint['save_freq']
-            del checkpoint['save_freq']
-        else:
-            SAVE_FREQ = 1
-
-        assert len(checkpoint) == 0, checkpoint
-
-        print("Loading checkpoint, ignoring other args")
-
-
-    if args.lambda_style:
-        LAMBD_STYLE = args.lambda_style
-    if args.lr:
-        LR = args.lr
-
-    encoder = VGG19Encoder().to(DEVICE)
-    decoder = Decoder().to(DEVICE)
-    if decoder_state_dict is not None:
-        decoder.load_state_dict(decoder_state_dict)
-
-    encoder.train()
-    decoder.train()
-
-    print(encoder)
-    print(decoder)
-    print("Created models")
-
-    transform = get_transforms(CROP)
-    dataset = StyleTransferDataset(COCO_PATH, COCO_LABELS_PATH,\
-                                   WIKIART_PATH, length = DATASET_LENGTH,
-                                   transform = transform, rng_seed = SEED)
-    transform = get_transforms(False)
-    val_dataset = StyleTransferDataset(COCO_PATH, COCO_LABELS_PATH,\
-                                   WIKIART_PATH, length = DATASET_LENGTH,
-                                   transform = transform, rng_seed = SEED)
-
-    dataloader = DataLoader(dataset, batch_size = BATCH_SIZE, \
-                            num_workers = NUM_WORKERS, shuffle = True)
-    val_dataloader = DataLoader(val_dataset, batch_size = 1, \
-                                num_workers = 1, shuffle = True)
-    print("Created dataloader")
-
-    if args.lr:
-        LR = args.lr
-    optimizer = torch.optim.Adam(params = decoder.parameters(), lr = LR)
-    if optimizer_state_dict is not None:
-        optimizer.load_state_dict(optimizer_state_dict)
-
-    #scheduler = ReduceLROnPlateau(optimizer, patience = 30,
-    #                              verbose = True, threshold = 0)
-    scheduler = StepLR(optimizer, step_size = args.scheduler_step,\
-                       gamma = 0.1)
-    if scheduler_state_dict is not None:
-        scheduler.load_state_dict(scheduler_state_dict)
-
-
-
-    writer = SummaryWriter(comment = args.comment)
-    writer.add_text('batch size', str(BATCH_SIZE), 0)
-    writer.add_text('save freq', str(SAVE_FREQ), 0)
-    writer.add_text('is cuda ', str(args.cuda), 0)
-    writer.add_text('dataset length ', str(DATASET_LENGTH), 0)
-    writer.add_text('num workers ', str(NUM_WORKERS), 0)
-    writer.add_text('num epochs ', str(NUM_EPOCHS), 0)
-    writer.add_text('lambd style', str(LAMBD_STYLE), 0)
-    writer.add_text('lambd content', str(LAMBD_CONTENT), 0)
-    writer.add_text('encoder', repr(encoder), 0)
-    writer.add_text('decoder', repr(decoder), 0)
-    writer.add_text('lr', str(LR), 0)
-
-    run = time.time()
-    os.makedirs(f"demo/{run}")
+def train(encoder, decoder, dataloader, val_dataloader, optimizer, scheduler, args, writer, saved_epoch, run, device):
     loss = None
-    for epoch in range(saved_epoch, NUM_EPOCHS):
-        if SAVE_FREQ != 0 and epoch % SAVE_FREQ == 0 and False:
+    for epoch in range(saved_epoch, args.num_epochs):
+        if args.save_freq != 0 and epoch % args.save_freq == 0 and False:
             torch.save({
                 'decoder_state_dict': decoder.state_dict(),
                 'optimizer_state_dict': optimizer.state_dict(),
                 'scheduler_state_dict': scheduler.state_dict(),
                 'epoch': epoch, 'loss': loss,
-                'num_workers': NUM_WORKERS, 'device': DEVICE,
-                'batch_size': BATCH_SIZE, 'dataset_length': DATASET_LENGTH,
-                'num_epochs': NUM_EPOCHS, 'lr': LR,
-                'lambda_style': LAMBD_STYLE, 'lambda_content': LAMBD_CONTENT,
-                'seed': SEED, 'coco_path': COCO_PATH,
-                'coco_labels_path': COCO_LABELS_PATH, 'wiki_path': WIKIART_PATH,
+                'num_workers': args.num_workers, 'device': device,
+                'batch_size': args.batch_size, 'dataset_length': args.dataset_length,
+                'num_epochs': args.num_epochs, 'lr': args.lr,
+                'lambda_style': args.lambda_style, 'lambda_content': args.lambda_content,
+                'seed': args.seed, 'coco_path': args.coco_path,
+                'coco_labels_path': args.coco_labels_path, 'wiki_path': args.wikiart_path,
             }, f"demo/{run}/{epoch}.pt")
 
-        loss = train_epoch_style_loss(encoder, decoder, dataloader, \
+        loss = train_epoch_style_loss(args, encoder, decoder, dataloader, \
                                       val_dataloader, optimizer,\
-                                      epoch, writer, run)
+                                      epoch, writer, run, device)
         scheduler.step()
 
 
-def train_epoch_style_loss(encoder, decoder, dataloader,\
+def train_epoch_style_loss(args, encoder, decoder, dataloader,\
                            val_dataloader, optimizer, \
-                           epoch_num, writer, run):
+                           epoch_num, writer, run, device):
     global g_batch_size
 
     encoder.train()
@@ -229,7 +50,7 @@ def train_epoch_style_loss(encoder, decoder, dataloader,\
     total_loss = 0
     num_iters = len(dataloader)
     if isinstance(dataloader.dataset, IterableStyleTransferDataset):
-        num_iters = ceil(len(dataloader) / BATCH_SIZE)
+        num_iters = ceil(len(dataloader) / args.batch_size)
         print(num_iters)
     print(num_iters)
 
@@ -237,15 +58,16 @@ def train_epoch_style_loss(encoder, decoder, dataloader,\
                              total = num_iters, dynamic_ncols = True)
     for i, (content_image, style_image) in progress_bar:
 
-        content_image = content_image.to(DEVICE)
-        style_image = style_image.to(DEVICE)
+        content_image = content_image.to(device)
+        style_image = style_image.to(device)
         g_batch_size, _, w, h = content_image.shape
         # assert content_image.shape == style_image.shape
 
         optimizer.zero_grad()
         style_loss, content_loss, stylized = \
                 get_style_transfer_loss(encoder, decoder, \
-                                              content_image, style_image)
+                                        content_image, style_image,\
+                                        args.lambda_content, args.lambda_style)
         full_loss = style_loss + content_loss
         full_loss.backward()
 
@@ -261,8 +83,9 @@ def train_epoch_style_loss(encoder, decoder, dataloader,\
         if epoch_num % 20 == 0:
             imgs_per_epoch = num_iters // 500
             img_num = imgs_per_epoch * epoch_num + i // 500
-            #validate_style_loss(encoder, decoder, val_dataloader, img_num, writer)
-            validate_style_loss(encoder, decoder, val_dataloader, epoch_num, writer)
+            validate_style_loss(encoder, decoder, val_dataloader,\
+                                epoch_num, writer, device)
+
             encoder.train()
             decoder.train()
 
@@ -278,13 +101,13 @@ def train_epoch_style_loss(encoder, decoder, dataloader,\
 def prep_img_for_tb(image):
     copy = image.cpu().detach().clone()
     clamp = torch.clamp(copy, 0, 1)
-    # assert clamp.shape == (3, 256, 256)
 
     return clamp
 
 
-def get_style_transfer_loss(encoder, decoder, content_image, style_image):
-    assert content_image.shape == (g_batch_size, 3, 256, 256)
+def get_style_transfer_loss(encoder, decoder, content_image, style_image,\
+                            lambda_content, lambda_style):
+    assert_shape(content_image, (g_batch_size, 3, 256, 256))
 
     style_features = encoder(style_image)
     content_features = encoder(content_image)
@@ -298,7 +121,7 @@ def get_style_transfer_loss(encoder, decoder, content_image, style_image):
     content_loss = compute_content_loss(features_of_stylized[-1],\
                                         stylized_features)
 
-    return style_loss * LAMBD_STYLE, content_loss * LAMBD_CONTENT,\
+    return style_loss * lambda_style, content_loss * lambda_content,\
                                      stylized_images
 
 
@@ -422,15 +245,15 @@ def show_tensor(tensor, num, run, info = ""):
     plt.imshow(image)
     plt.savefig(f"demo/{run}/{num}{info}.png")
 
-def validate_style_loss(encoder, decoder, dataloader, epoch_num, writer):
+def validate_style_loss(encoder, decoder, dataloader, epoch_num, writer, device):
     global g_batch_size
 
     encoder.eval()
     decoder.eval()
 
     content_image, style_image = next(iter(dataloader))
-    content_image = content_image.to(DEVICE)
-    style_image = style_image.to(DEVICE)
+    content_image = content_image.to(device)
+    style_image = style_image.to(device)
     g_batch_size, _, w, h = content_image.shape
 
     stylized = style_transfer(encoder, decoder, content_image, style_image)
@@ -443,6 +266,9 @@ def validate_style_loss(encoder, decoder, dataloader, epoch_num, writer):
     writer.add_image('stylized', f_image, epoch_num)
 
 
+def assert_shape(tensor, expected):
+    msg = f"expected {expected} actual {tensor.shape}"
+    assert tensor.shape == expected, msg
 
 
 if __name__ == "__main__":
